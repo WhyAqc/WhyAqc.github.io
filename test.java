e2979cc547f1428aeeb9fb881f6381d80aa979f9
WhyAqc <745225422@qq.com>
2017-04-26 21:48:59 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("hi");
+		int[] a = new int[10];
+		for (int i = 0; i < 10; i++) {
+			a[i] = i;
+			System.out.println(a[i]);
+		}
+	}
+}
